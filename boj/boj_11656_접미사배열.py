eaa009b7332ae428b25c812256a26ade2663c2be
SKH <kevinsung123@naver.com>
2020-03-08 01:26:48 +0900

    add boj11656 & leetcode 151 reverse words in string

--- boj/boj_11656_접미사배열.py
+++ boj/boj_11656_접미사배열.py
@@ -1,21 +1,16 @@
 import sys
-
-
 def solve():
     global S
     S = list(S)
     ans = []
     while len(S) > 0:
-        print(''.join(S))
         ans.append(''.join(S))
         S.pop(0)
     ans.sort()
-
     for i in ans:
-        pass
-       # print(i)
+       print(i)
 
 
 if __name__ == '__main__':
-    S = sys.stdin.readline()
+    S = sys.stdin.readline().strip()
     solve()
