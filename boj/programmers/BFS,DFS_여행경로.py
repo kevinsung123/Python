b316a3becb3b6149f0c1b10bb227c93a5d7f1171
SKH <kevinsung123@naver.com>
2020-06-30 01:16:43 +0900

    add

--- boj/programmers/BFS,DFS_여행경로.py
+++ boj/programmers/BFS,DFS_여행경로.py
@@ -1,18 +1,30 @@
-from collections import deque,defaultdict
+from collections import deque, defaultdict
 from heapq import *
 
 
+def dfs(cur,len):
+    global chk, answer, new_tickets
+    if len == N+1 :
+        return
+    if len>N:
+        return
+    for idx, there in enumerate(new_tickets):
+        if cur == there[0] and chk[idx] == 0:
+            chk[idx] = 1
+            dfs(there[1],len+1)
+            answer.append(there[1])
+
 
 def solution(tickets):
+    global answer, chk, N,new_tickets
     answer = []
-    routes = defaultdict(list)
-
-    for start, end in tickets:
-        routes[start].append(end)
-    # 2. 시작점 - [끝점] 역순으로 정렬
-    print(routes)
-
-    return answer
+    chk = [False] * len(tickets)
+    N = len(tickets)
+    new_tickets = sorted(tickets, key=lambda x: (x[0], x[1]))
+    print(new_tickets)
+    dfs("ICN",0)
+    answer.append("ICN")
+    return answer[::-1]
 
 
 if __name__ == '__main__':
