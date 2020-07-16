cabcb029615f17d291632ca94d91d4e119e1e344
SKH <kevinsung123@naver.com>
2020-07-16 23:16:40 +0900

    Add programmers & boj floyd

--- boj/programmers/그래프_순위.py
+++ boj/programmers/그래프_순위.py
@@ -3,31 +3,34 @@ from collections import deque
 
 def solution(n, results):
     answer = 0
-    adj = [[] for i in range(n + 1)]
-    indgree = [0 for i in range(n + 1)]
+    INF = int(1e9)
+    dist = [[0 if i == j else INF for j in range(n+1)] for i in range(n+1)]
     for i in results:
-        src, dest = i
-        adj[src].append(dest)
-        indgree[dest] += 1
-    q = deque()
-    print(adj)
-    # 정렬시작
-    for i in range(n + 2):
-        if indgree[i+1] == 0:
-            q.append(i+1)
-    ans = []
-    road = [0 for i in range(n+1)]
-    print(q)
-    for i in range(n + 1):
-        if len(q) == 0:
-            answer = 0
-            break
-        cur = q.popleft()
-        road[i] = cur + 1
-        for i in adj[cur]:
-            if --indgree[i] == 0:
-                q.append(i)
-    print(road)
+        win, lose = i
+        dist[win][lose] = 1
+    # floyd
+    for k in range(1,n + 1):
+        for i in range(1,n + 1):
+            for j in range(1, n + 1):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+    flag = [True for i in range(n+1)]
+    # print(flag)
+    # for i in range(1,n+1):
+    #     for j in range(1,n+1):
+    #         print(dist[i][j],end="\t")
+    #     print()
+    # 승패를 알 수 없는 경우
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if i == j:
+                continue
+            if dist[i][j] == INF:
+                if dist[j][i] == INF:
+                    flag[i] = False
+                    break
+    for i in range(1, n + 1):
+        if flag[i] is True:
+            answer += 1
     return answer
 
 
